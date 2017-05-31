103d87956a5e3fdfb7192b41d6b41b0c15bfd623
Alexander Boersch <aboersch@outlook.com>
2017-05-31 10:50:44 +0200

    Added Tests

---
+++ com.aboersch.PostProxy.Tests/ForwardingRequestBuilderTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Combinatorics.Collections;
+using System.Linq;
+
+namespace com.aboersch.PostProxy.Tests
+{
+    [TestClass]
+    public class ForwardingRequestBuilderTest
+    {
+        const string BASE_ADDRESS = "http://web.address/route";
+        const string PARAM_1 = "param1=param1value";
+        const string PARAM_2 = "param2=param2value";
+        const string FORWARDING_URL = "http://forward.url/forwardroute";
+        //$"url={Uri.EscapeDataString(FORWARDING_URL)}"
+        const string PARAM_FORWARDING_URL = "url=http%3A%2F%2Fforward.url%2Fforwardroute";
+
+        private Uri CreateRequestUrl(params string[] queryParameters)
+        {
+            return new Uri(CreateUrl(BASE_ADDRESS, queryParameters));
+        }
+
+        private string GetResultUrl(params string[] queryParameters)
+        {
+            return CreateUrl(FORWARDING_URL, queryParameters);
+        }
+
+        private string CreateUrl(string baseUrl, params string[] queryParameters)
+        {
+            return $"{baseUrl}{(queryParameters.Any() ? "?" : null) }{string.Join("&", queryParameters)}";
+        }
+
+        private void NoForwardingUrl(params string[] queryParameters)
+        {
+            try
+            {
+                ForwardingRequestBuilder.CreateUrl(CreateRequestUrl(queryParameters));
+                Assert.Fail("Should throw an exception");
+            }
+            catch { }
+        }
+
+        [TestMethod]
+        public void NoForwardingUrlTest()
+        {
+            NoForwardingUrl(PARAM_1);
+        }
+
+        static string[] ForwardingArray = new[] { PARAM_FORWARDING_URL };
+
+        public void ValidForwardingUrl(params string[] queryParameters)
+        {
+            Assert.AreEqual(
+                ForwardingRequestBuilder.CreateUrl(CreateRequestUrl(queryParameters)),
+                GetResultUrl(queryParameters.Except(ForwardingArray).ToArray()));
+        }
+
+        [TestMethod]
+        public void ValidForwardingUrlTest()
+        {
+            ValidForwardingUrl(PARAM_FORWARDING_URL, PARAM_1);
+        }
+
+        [TestMethod]
+        public void ParameterPermutationsTest()
+        {
+            var paramList = new List<string>(new[] { PARAM_1, PARAM_2, PARAM_FORWARDING_URL });
+            for (int i = 1; i < 4; i++)
+            {
+                var variations = new Variations<string>(paramList, i, GenerateOption.WithoutRepetition);
+                foreach (var variation in variations)
+                {
+                    var shouldFail = !variation.Contains(PARAM_FORWARDING_URL);
+                    var permutations = new Permutations<string>(variation, GenerateOption.WithoutRepetition);
+                    foreach (var permutation in permutations)
+                    {
+                        var queryParameters = permutation.ToArray();
+                        if (shouldFail)
+                            NoForwardingUrl(queryParameters);
+                        else
+                            ValidForwardingUrl(queryParameters);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ com.aboersch.PostProxy.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("com.aboersch.PostProxy.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alegri International Group")]
+[assembly: AssemblyProduct("com.aboersch.PostProxy.Test")]
+[assembly: AssemblyCopyright("Copyright © Alegri International Group 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("683bebf7-3d33-449c-8b2c-f5d2c6987a9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
